f4370cf12b952be878319ab96a611874ffcde2e5
Igor Koshelev <igor.koshelev440@gmail.com>
2021-02-21 12:46:11 +0300

    feat: упрощает работу с компонентами

--- src/components/Bar/index.ts
+++ src/components/Bar/index.ts
@@ -13,32 +13,33 @@ interface IConfig {
 }
 
 export default class BarClass extends CommonComponent {
-  template;
-
-  constructor(cfg?: IConfig) {
+  constructor(private cfg?: IConfig) {
     super();
-    if (cfg?.accent) this.addClass("bar--accent");
-    if (cfg?.underSlot) this.addClass("bar--shaped");
-    if (cfg?.small) this.addClass("bar--small");
-    if (cfg?.aboweSlotAlign === "left") this.addClass("bar--align-left");
-    if (cfg?.aboweSlotAlign === "right") this.addClass("bar--align-right");
+  }
 
-    const barTop = cfg?.topSlot
-      ? `<div class="bar__top headline">${cfg.topSlot}</div>`
+  render() {
+    if (this.cfg?.accent) this.addClass("bar--accent");
+    if (this.cfg?.underSlot) this.addClass("bar--shaped");
+    if (this.cfg?.small) this.addClass("bar--small");
+    if (this.cfg?.aboweSlotAlign === "left") this.addClass("bar--align-left");
+    if (this.cfg?.aboweSlotAlign === "right") this.addClass("bar--align-right");
+
+    const barTop = this.cfg?.topSlot
+      ? `<div class="bar__top headline">${this.cfg.topSlot}</div>`
       : "";
-    const barAbowe = cfg?.aboweSlot
-      ? `<div class="bar__abowe subhead">${cfg.aboweSlot}</div>`
+    const barAbowe = this.cfg?.aboweSlot
+      ? `<div class="bar__abowe subhead">${this.cfg.aboweSlot}</div>`
       : "";
-    const barBottom = cfg?.bottomSlot
-      ? `<div class="bar__bottom">${cfg.bottomSlot}</div>`
+    const barBottom = this.cfg?.bottomSlot
+      ? `<div class="bar__bottom">${this.cfg.bottomSlot}</div>`
       : "";
-    const barUnder = cfg?.underSlot
-      ? `<div class="bar__under body">${cfg.underSlot}</div>`
+    const barUnder = this.cfg?.underSlot
+      ? `<div class="bar__under body">${this.cfg.underSlot}</div>`
       : "";
 
-    this.template = `
+    return `
     <div class="bar ${this.getClasses()}" style="--height: ${
-      cfg?.height || 0
+      this.cfg?.height || 0
     }%;">
       ${barAbowe}
       <div class="bar__content">
--- src/components/CommonComponent.ts
+++ src/components/CommonComponent.ts
@@ -1,12 +1,32 @@
 export default abstract class CommonComponent {
-  abstract template: string;
+  // Abstract
+  protected abstract render(): string;
+
+  // Real
+  private _template: string = "";
   private _classes: string[] = [];
 
-  addClass(val: string): void {
+  // Instance
+  protected addClass(val: string): void {
     this._classes.push(val);
+  }
+
+  // Pub
+  pushClasses(val: string[]): this {
+    this._classes.push(...val);
+    this._template = this.render();
+
+    return this;
   }
 
   getClasses(): string {
     return this._classes.join(" ");
+  }
+
+  toString() {
+    if (this._template) return this._template;
+    this._template = this.render();
+
+    return this._template;
   }
 }
