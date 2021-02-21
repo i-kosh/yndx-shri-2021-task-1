cb203654138a51ab827352a145b2296ed0e6055b
Igor Koshelev <igor.koshelev440@gmail.com>
2021-02-21 18:54:16 +0300

    feat: добавляет компоненты для ладдера

---
+++ src/components/BarLadder/index.ts
@@ -0,0 +1,84 @@
+import "./style.scss";
+import CommonComponent from "../CommonComponent";
+import Bar from "../BarLadderItem";
+import UserCard from "../UserCard";
+
+interface ICfg {
+  bars: {
+    userName: string;
+    userSub: string;
+    userAvatar: string;
+    userMark?: string;
+    accent?: boolean;
+    place: number;
+  }[];
+  selectedUserIndex?: number;
+}
+
+export default class BarLadderClass extends CommonComponent {
+  constructor(private cfg: ICfg) {
+    super();
+  }
+
+  render() {
+    let sortFlag = 1;
+
+    const selectedUserBar =
+      this.cfg.selectedUserIndex && this.cfg.selectedUserIndex > 2
+        ? this.cfg.bars[this.cfg.selectedUserIndex]
+        : undefined;
+    const selectedUserCard = selectedUserBar
+      ? `${new UserCard({
+          avatar: selectedUserBar.userAvatar,
+          name: selectedUserBar.userName,
+          sub: selectedUserBar.userSub,
+          mark: selectedUserBar.userMark,
+        }).pushClasses(["ladder__selected-user"])}`
+      : null;
+    const selectedUserContent = `
+    <div class="ladder__selected">
+        ${selectedUserCard}
+        <div class="divider divider--small divider--black divider--no-opacity ladder__selected-divider"></div>
+        <span class="headline ladder__selected-headline">${selectedUserBar?.place}</span>
+    </div>
+    `;
+
+    const bars = this.cfg.bars.map((bar, index) => {
+      sortFlag = sortFlag * -1;
+      if (index > 4) return "";
+
+      if (
+        this.cfg.selectedUserIndex &&
+        this.cfg.selectedUserIndex > 4 &&
+        index === 4
+      ) {
+        bar = this.cfg.bars[this.cfg.selectedUserIndex];
+      }
+
+      return new Bar({
+        avatar: bar.userAvatar,
+        name: bar.userName,
+        sub: bar.userSub,
+        topSlot: `${bar.place}`,
+        accent: bar.accent,
+        mark: bar.userMark,
+        order: index * sortFlag,
+        bottomSlot:
+          selectedUserCard && index === 0
+            ? `${selectedUserContent}`
+            : undefined,
+      }).pushClasses(["ladder__bar"]);
+    });
+
+    const barsString = bars
+      .reverse()
+      .map((bar) => `${bar}`)
+      .join("");
+
+    return `
+    <div class="ladder ${this.getClasses()}">
+      ${barsString}
+    </div>
+    `;
+  }
+}
---
+++ src/components/BarLadderItem/index.ts
@@ -0,0 +1,56 @@
+import CommonComponent from "../CommonComponent";
+import Bar from "../Bar";
+import UserCard from "../UserCard";
+
+interface ICfg {
+  avatar: string;
+  name: string;
+  sub: string;
+  mark?: string;
+  topSlot: string;
+  bottomSlot?: string;
+  accent?: boolean;
+  order?: number;
+}
+
+export default class BarLadderItemClass extends CommonComponent {
+  constructor(private cfg: ICfg) {
+    super();
+  }
+
+  render() {
+    const styleOrder =
+      this.cfg?.order != null ? `order: ${this.cfg.order};` : "";
+
+    const styleZindex =
+      this.cfg?.order != null
+        ? `z-index: ${10 - Math.abs(this.cfg.order)};`
+        : "";
+
+    const user = new UserCard({
+      avatar: this.cfg.avatar,
+      name: this.cfg.name,
+      sub: this.cfg.sub,
+      mark: this.cfg.mark,
+    });
+    const bar = new Bar({
+      topSlot: this.cfg.topSlot,
+      bottomSlot: this.cfg.bottomSlot,
+      aboweSlot: `${user}`,
+      accent: this.cfg.accent,
+      aboweSlotAlign:
+        this.cfg.order && this.cfg.order < 0
+          ? "left"
+          : this.cfg.order && this.cfg.order > 0
+          ? "right"
+          : undefined,
+      order: this.cfg.order,
+    });
+
+    return `
+    <div class="ladder-bar-item ${this.getClasses()}" style="${styleOrder} ${styleZindex}">
+      ${bar}
+    </div>
+    `;
+  }
+}
