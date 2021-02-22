4e80a824eebf28569fe98d7cac320a2fed8df6d8
Igor Koshelev <igor.koshelev440@gmail.com>
2021-02-22 18:05:16 +0300

    feat: компонент для отрисовки голосования

---
+++ src/views/Vote/index.ts
@@ -0,0 +1,53 @@
+import VoteBoard from "../../components/VoteBoard";
+import Common from "../CommonView";
+import Container from "../../components/Container";
+import MainHeading from "../../components/MainHeading";
+import getAvatar from "../../avatars";
+
+interface VoteSlide {
+  emoji: string;
+  subtitle: string;
+  title: string;
+  selectedUserId: number;
+  users: {
+    id: number;
+    name: string;
+    avatar: string;
+    valueText: string;
+  }[];
+}
+
+export default class Vote extends Common {
+  constructor(private data: Partial<VoteSlide>) {
+    super();
+  }
+
+  render() {
+    const heading = new MainHeading({
+      title: this.data.title || "",
+      subtitle: this.data.subtitle,
+    });
+
+    return `
+      ${new Container(
+        `
+          ${heading}
+          ${new VoteBoard({
+            users:
+              this.data.users?.map((user) => {
+                return {
+                  avatar: getAvatar(user.avatar),
+                  name: user.name,
+                  id: user.id,
+                };
+              }) || [],
+            selectedUserIndex: this.data.users?.findIndex(
+              (user) => user.id === this.data.selectedUserId
+            ),
+          })}
+        `,
+        "main"
+      )}
+    `;
+  }
+}
